6af04c48117d48d968316b7d3f7917fa01386a0b
Spiroman <spiroman@gmail.com>
2019-11-03 03:47:48 +0200

    Update deployment_script.sh

--- Vagrant/deployment_script.sh
+++ Vagrant/deployment_script.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-if ! type $"python" > dev/null; then
-	yes Y | sudo apt install python
-fi
-sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python get-pip.py
-sudo pip install -U Flask
-sudo mkdir -p /var/webapp && cp /tmp/webapp.py /var/webapp
-cd /var/webapp && export FLASK_APP=webapp.py && flask run
+error_report(){
+	echo "Execution stopped, details can be found at /tmp/errorlog.txt"
+	"$1" >& /tmp/errorlog.txt
+}
+
+wrapper(){
+	eval $1
+	if [ $? -ne 0 ]; then
+		error_report "$1"
+		exit
+	fi
+}
+
+main(){
+	if ! type $"python" > dev/null; then
+		wrapper "yes Y | sudo apt install python"
+	fi
+	wrapper "sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py"
+	wrapper "sudo python get-pip.py"
+	wrapper "sudo pip install -U Flask"
+	wrapper "sudo mkdir -p /var/webapp && cp /tmp/webapp.py /var/webapp"
+	wrapper "cd /var/webapp && export FLASK_APP=webapp.py && flask run"
+}
+
+main
